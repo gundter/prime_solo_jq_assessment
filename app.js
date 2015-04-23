1bc52e1911022b4a8f596e8ceaa9a57474227457
Terry Gunderson <tgunderson25@gmail.com>
2015-04-23 16:49:03 -0500

    Finished project

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Prime on 4/23/15.
+ */
+var counter = 0;
+
+$(document).ready(function(){
+   $('button').click(function(){
+       counter++;
+       $('.container').append("<div class='row' id="+ counter + ">Line #" + counter + "<button class='colorButton'>Change Color</button><button class='removeButton'>Remove</button></div>");
+
+       var added = $("#" + counter);
+       added.hide().slideDown("fast");
+   });
+
+    $('.container').on("click", ".row .colorButton", function(){
+       $(this).parent().toggleClass('selected');
+    });
+
+    $('.container').on("click", ".row .removeButton", function(){
+       $(this).parent().slideUp("fast", function(){
+           $(this).remove();
+       });
+    });
+});
